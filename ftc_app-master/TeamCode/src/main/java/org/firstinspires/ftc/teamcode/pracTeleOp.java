be53d2c231226c5969430155490a95c17c7956a0
Arnav Suri <arnavsuri99@gmail.com>
2019-09-17 21:34:07 -0700

    Direction Set

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pracTeleOp.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pracTeleOp.java
@@ -3,23 +3,34 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.Range;
 
 public class pracTeleOp extends OpMode {
 
-    private DcMotor topLeftMotor;
-    private DcMotor topRightMotor;
-    private DcMotor bottomRightMotor;
-    private DcMotor bottomLeftMotor;
+    private DcMotor fl;
+    private DcMotor fr;
+    private DcMotor br;
+    private DcMotor bl;
     private Gamepad gamepad;
 
-    public void init(){
-        topLeftMotor = hardwareMap.dcMotor.get("topLeftMotor;");
-        topRightMotor = hardwareMap.dcMotor.get("topRightMotor");
-        bottomRightMotor = hardwareMap.dcMotor.get("bottomRightMotor");
-        bottomLeftMotor = hardwareMap.dcMotor.get("bottomLeftMotor");
-    }
+    double speed = -gamepad.left_stick_y;
+    double strafe = gamepad.left_stick_x;
+    double turn = gamepad.right_stick_x;
 
+
+    public void init() {
+        fl = hardwareMap.dcMotor.get("fl");
+        fr = hardwareMap.dcMotor.get("fr");
+        br = hardwareMap.dcMotor.get("br");
+        bl = hardwareMap.dcMotor.get("bl");
+
+
+    }
     public void loop(){
+        fl.setPower(Range.clip(speed+turn-strafe,-1,1));
+        fr.setPower(Range.clip(speed-turn+strafe,-1,1));
+        br.setPower(Range.clip(speed-turn-strafe,-1,1));
+        bl.setPower(Range.clip(speed+turn+strafe,-1,1));
 
     }
 }
